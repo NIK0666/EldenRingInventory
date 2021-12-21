0ef6d6ad196c81ff58bb7321bcd35e480cd6978b
Nikolay Unru <nikolay.unru@mytona.com>
2021-12-21 10:37:18 +0600

    Equipment and Inventory panels in progress

---
+++ Source/TestEldenRing/Inventory/UMG/EquipmentPanelWidget.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "EquipmentPanelWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTELDENRING_API UEquipmentPanelWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
